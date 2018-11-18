f520e0f78ec9aebb7a6e5e00f6c8d037542b7018
Murillo Grubler <msgrubler@gmail.com>
2018-11-18 20:06:43 -0200

    Adding filters

--- src/api/views.py
+++ src/api/views.py
@@ -1,9 +1,15 @@
 # Create your views here.
 from rest_framework import generics
+from rest_framework import filters
 from .models import Video
 from .serializers import VideoSerializer
+
 
 # Create your views here.
 class VideoList(generics.ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    filter_backends = (filters.SearchFilter,filters.OrderingFilter,)
+    search_fields = ('id', 'title')
+    ordering_fields = '__all__'
+    
