78928d7964afa73d1c67ce0417edbf5a410aa536
Murillo Grubler <msgrubler@gmail.com>
2018-11-25 11:28:05 -0200

    Update Video model

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from datetime import datetime  
 
 # Create your models here.
 class Video(models.Model):
     class Meta:
         db_table = 'video'
     title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     seconds = models.IntegerField()
+    created = models.DateTimeField(blank=True)
     def __str__(self):
         return self.title
